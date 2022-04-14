96856884edd056c73320402962a23881050d4c75
rkozitsa <rkozitsa@paloaltonetworks.com>
2022-04-14 23:02:07 +0100

    wip

--- src/app/shared/components/images-grid/images-grid.component.ts
+++ src/app/shared/components/images-grid/images-grid.component.ts
@@ -1,14 +1,14 @@
-import {Component, ChangeDetectionStrategy, Input} from '@angular/core';
+import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 
-import {StoreItemInterface} from '../store-item/store-item.interface';
+import { StoreItemInterface } from '../store-item/store-item.interface';
 
 @Component({
-    selector: 'app-images-grid',
-    templateUrl: './images-grid.component.html',
-    styleUrls: ['./images-grid.component.scss'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+  selector: 'app-images-grid',
+  templateUrl: './images-grid.component.html',
+  styleUrls: ['./images-grid.component.scss'],
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
 export class ImagesGridComponent {
-    @Input() gridItems: StoreItemInterface[] = [];
-    @Input() gridStyle: object;
+  @Input() gridItems: StoreItemInterface[] = [];
+  @Input() gridStyle: object;
 }

973013e7bf1e0ea4a38ad487bfdb88328e6c9e73
rkozitsa <rkozitsa@paloaltonetworks.com>
2022-04-15 21:46:20 +0100

    wip error form

--- src/app/shared/components/input-text/input-text.component.ts
+++ src/app/shared/components/input-text/input-text.component.ts
@@ -1,5 +1,5 @@
-import { Component, ChangeDetectionStrategy, forwardRef } from '@angular/core';
-import { ControlValueAccessor, NG_VALUE_ACCESSOR } from '@angular/forms';
+import { Component, ChangeDetectionStrategy, Self, Optional, Input } from '@angular/core';
+import { ControlValueAccessor, NgControl } from '@angular/forms';
 
 import { ValueAccessor } from '../../classes/value-accessor';
 
@@ -7,13 +7,22 @@ import { ValueAccessor } from '../../classes/value-accessor';
   selector: 'app-input-text',
   templateUrl: './input-text.component.html',
   styleUrls: ['./input-text.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush,
-  providers: [
-    {
-      provide: NG_VALUE_ACCESSOR,
-      multi: true,
-      useExisting: forwardRef(() => InputTextComponent)
-    }
-  ]
+  changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class InputTextComponent extends ValueAccessor implements ControlValueAccessor {}
+export class InputTextComponent extends ValueAccessor implements ControlValueAccessor {
+  @Input() label: string;
+
+  constructor(@Self() @Optional() private ngControl: NgControl) {
+    super();
+    // tslint:disable-next-line:no-unused-expression
+    this.ngControl && (this.ngControl.valueAccessor = this);
+  }
+
+  get showError(): boolean {
+    if (!this.ngControl) {
+      return false;
+    }
+
+    return this.ngControl.dirty && this.ngControl.invalid;
+  }
+}

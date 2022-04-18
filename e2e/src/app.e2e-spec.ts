043ab0d0d5dd273c8a526ab65e631f6347174e28
rkozitsa <rkozitsa@paloaltonetworks.com>
2022-04-18 01:13:10 +0100

    add buildspec.yml file

--- e2e/src/app.e2e-spec.ts
+++ e2e/src/app.e2e-spec.ts
@@ -10,7 +10,7 @@ describe('workspace-project App', () => {
 
   it('should display welcome message', () => {
     page.navigateTo();
-    expect(page.getTitleText()).toEqual('Welcome to website-project!');
+    expect(page.getTitleText()).toEqual('Welcome to painting-store!');
   });
 
   afterEach(async () => {

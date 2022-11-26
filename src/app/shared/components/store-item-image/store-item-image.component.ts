3d1bed7108c3f34a0acf35b134afb1a5d56fa64a
rkozitsa <rkozitsa@paloaltonetworks.com>
2022-11-26 19:24:09 +0000

    fixed loader position

--- src/app/shared/components/store-item-image/store-item-image.component.ts
+++ src/app/shared/components/store-item-image/store-item-image.component.ts
@@ -1,11 +1,12 @@
 import { Component, ChangeDetectionStrategy, Input } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
 
+
 @Component({
   selector: 'app-store-item-image',
   templateUrl: './store-item-image.component.html',
   styleUrls: ['./store-item-image.component.scss'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class StoreItemImageComponent {
   @Input() imgPath: string;

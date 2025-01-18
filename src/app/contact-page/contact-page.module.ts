5032e9a0a474c05b9dfbfefdf3e206c855d6b1f3
rkozitsa <rkozitsa@paloaltonetworks.com>
2025-01-18 14:30:19 +0200

    update angular WIP

--- src/app/contact-page/contact-page.module.ts
+++ src/app/contact-page/contact-page.module.ts
@@ -2,14 +2,11 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterModule } from '@angular/router';
 import { ReactiveFormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { provideHttpClient, withInterceptorsFromDi } from '@angular/common/http';
 
 import { ContactPageComponent } from './contact-page.component';
 import { contactPageRoutes } from './contact-page.routing';
 import { SharedModule } from '../shared/shared.module';
 
-@NgModule({
-  declarations: [ContactPageComponent],
-  imports: [CommonModule, RouterModule.forChild(contactPageRoutes), ReactiveFormsModule, HttpClientModule, SharedModule]
-})
+@NgModule({ declarations: [ContactPageComponent], imports: [CommonModule, RouterModule.forChild(contactPageRoutes), ReactiveFormsModule, SharedModule], providers: [provideHttpClient(withInterceptorsFromDi())] })
 export class ContactPageModule {}

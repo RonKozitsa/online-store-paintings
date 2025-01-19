1e9f71d89224838ec879efcac435e928753da4b1
rkozitsa <rkozitsa@paloaltonetworks.com>
2025-01-19 12:23:01 +0200

    update angular WIP

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,16 @@
 import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { trigger } from '@angular/animations';
 import { BehaviorSubject } from 'rxjs';
+import {RouterLink, RouterOutlet} from '@angular/router';
+import { AsyncPipe, NgIf } from '@angular/common';
 
 import { animations } from './shared/animations/animations';
 import { ButtonType } from './shared/components/app-button/app-button.interface';
 import { ThemeService } from './shared/services/theme.service';
 import { BreakpointObserverService } from './shared/services/breakpoint-observer.service';
 import { NavigationHeaderComponent } from './shared/components/navigation-header/navigation-header.component';
-import { AsyncPipe, NgIf } from '@angular/common';
 import { ClickOutsideDirective } from './shared/directives/click-outside.directive';
 import { AppButtonComponent } from './shared/components/app-button/app-button.component';
-import { RouterOutlet } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -18,7 +18,7 @@ import { RouterOutlet } from '@angular/router';
   styleUrls: ['./app.component.scss'],
   changeDetection: ChangeDetectionStrategy.OnPush,
   standalone: true,
-  imports: [NavigationHeaderComponent, AppButtonComponent, AsyncPipe, ClickOutsideDirective, RouterOutlet, NgIf],
+  imports: [NavigationHeaderComponent, AppButtonComponent, AsyncPipe, ClickOutsideDirective, RouterOutlet, NgIf, RouterLink],
   animations: [trigger('slideInOut', animations.slideInOut)]
 })
 export class AppComponent {
